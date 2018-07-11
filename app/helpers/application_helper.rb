93ed722c0474a19014a1c95fcb9a1f951ed9945f
sugyan <sugi1982@gmail.com>
2018-07-12 01:04:36 +0900

    Update app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def javascript_include_tag(*sources)
+    if Rails.env.development?
+      opts = {
+        src: 'http://localhost:8080/bundle.js'
+      }
+      return content_tag(:script, '', opts)
+    end
+    super(*sources)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'root#index'
+  get '/result', to: redirect('/')
+  get '/result/:key', to: 'root#result'
+
+  namespace :api do
+    get  'timetable', constraints: { format: :json }
+    post 'generate'
+    post 'tweet'
+  end
 end
